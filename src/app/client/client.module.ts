c369be14e3fc747ebbcbb6edab4f0e87055cbf11
Abderrazak Eldouqe <abderrazak.eldouqe1@gmail.com>
2021-05-15 14:20:27 +0000

    Add Design Client Module

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -3,17 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
 import { SharedModule } from '../_shared/shared.module';
-import { HeaderComponent } from './_shared/header/header.component';
 import { FooterComponent } from './_shared/footer/footer.component';
 import { LayoutComponent } from './_shared/layout/layout.component';
 import { CardShowComponent } from './_shared/card-show/card-show.component';
+import { NavbarComponent } from './_shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     FooterComponent,
     LayoutComponent,
-    CardShowComponent
+    CardShowComponent,
+    NavbarComponent,
   ],
   imports: [CommonModule, SharedModule, ClientRoutingModule],
 })
