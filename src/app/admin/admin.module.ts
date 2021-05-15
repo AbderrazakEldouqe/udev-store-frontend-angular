71c955cd2e21888c7968a22f5371fe9eab027783
Abderrazak Eldouqe <abderrazak.eldouqe1@gmail.com>
2021-05-15 15:45:08 +0000

    Add Admin Design

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,12 +6,14 @@ import { SharedModule } from '../_shared/shared.module';
 import { LayoutComponent } from './_shared/layout/layout.component';
 import { HeaderComponent } from './_shared/header/header.component';
 import { FooterComponent } from './_shared/footer/footer.component';
+import { NavbarComponent } from './_shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [CommonModule, SharedModule, AdminRoutingModule],
 })
